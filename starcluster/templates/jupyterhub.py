bdae07260de7e1721c51f4c30e1b3b158517d7d2
Daniel Treiman <dtreiman@alivecor.com>
2020-04-27 16:16:23 -0700

    Updates to use callable to provide profiles to wrap spawner.

--- starcluster/templates/jupyterhub.py
+++ starcluster/templates/jupyterhub.py
@@ -102,70 +102,76 @@ c.Spawner.environment = dict(
 c.Spawner.http_timeout = 120
 
 
-QHOST_PATH = '/opt/sge6/bin/linux-x64/qhost'
+def get_node_profiles()
+    QHOST_PATH = '/opt/sge6/bin/linux-x64/qhost'
+    
+    ENV = dict(os.environ)
+    ENV['HOME'] = '/home/sgeadmin'
+    ENV['SGE_CELL'] = 'default'
+    ENV['SGE_EXECD_PORT'] = '63232'
+    ENV['SGE_QMASTER_PORT'] = '63231'
+    ENV['SGE_ROOT'] = '/opt/sge6'
+    ENV['SGE_CLUSTER_NAME'] = 'starcluster'
+    
+    command = '{} -xml -q'.format(QHOST_PATH)
+    result_xml = subprocess.check_output([command], env=ENV, shell=True)
+    hosts_element = xml.etree.ElementTree.fromstring(result_xml)
+    node_profiles = []
+    for host_element in hosts_element:
+        if host_element.get('name') == 'global':
+            continue
+        name = host_element.get('name')
+        queues = []
+        for host_value in host_element:
+            if host_value.tag == 'queue':
+                queue_name = host_value.get('name')
+                slots = 0
+                for qv in host_value:
+                    if qv.get('name') == 'slots':
+                        slots = int(qv.text)
+                if slots > 0:
+                    queues.append(queue_name)
+        queue = 'cpu.q'
+        if 'mem.q' in queues:
+            queue = 'mem.q'
+        if 'gpu.q' in queues:
+            queue = 'gpu.q'
+        queue_type = queue.split('.')[0]
+        node_queue = '{}@{}'.format(queue, name)
+        node_profiles.append(
+            (u'{} ({})'.format(name, queue_type), u'{}_{}'.format(queue.replace('.', '_'), name.replace('.', '_')), GridengineSpawner, dict(
+                batch_submit_cmd='sudo -u {{username}} -E /opt/sge6/bin/linux-x64/qsub -q {}'.format(node_queue),
+                batch_query_cmd='sudo -u {{username}} -E /opt/sge6/bin/linux-x64/qstat -q {} -xml'.format(node_queue),
+                batch_cancel_cmd='sudo -u {username} -E /opt/sge6/bin/linux-x64/qdel {job_id}',
+                hub_connect_ip=hub_ip_address))
+        )
+    return node_profiles
 
-ENV = dict(os.environ)
-ENV['HOME'] = '/home/sgeadmin'
-ENV['SGE_CELL'] = 'default'
-ENV['SGE_EXECD_PORT'] = '63232'
-ENV['SGE_QMASTER_PORT'] = '63231'
-ENV['SGE_ROOT'] = '/opt/sge6'
-ENV['SGE_CLUSTER_NAME'] = 'starcluster'
 
-command = '{} -xml -q'.format(QHOST_PATH)
-result_xml = subprocess.check_output([command], env=ENV, shell=True)
-hosts_element = xml.etree.ElementTree.fromstring(result_xml)
-node_profiles = []
-for host_element in hosts_element:
-    if host_element.get('name') == 'global':
-        continue
-    name = host_element.get('name')
-    queues = []
-    for host_value in host_element:
-        if host_value.tag == 'queue':
-            queue_name = host_value.get('name')
-            slots = 0
-            for qv in host_value:
-                if qv.get('name') == 'slots':
-                    slots = int(qv.text)
-            if slots > 0:
-                queues.append(queue_name)
-    queue = 'cpu.q'
-    if 'mem.q' in queues:
-        queue = 'mem.q'
-    if 'gpu.q' in queues:
-        queue = 'gpu.q'
-    queue_type = queue.split('.')[0]
-    node_queue = '{}@{}'.format(queue, name)
-    node_profiles.append(
-        (u'{} ({})'.format(name, queue_type), u'{}_{}'.format(queue.replace('.', '_'), name.replace('.', '_')), GridengineSpawner, dict(
-            batch_submit_cmd='sudo -u {{username}} -E /opt/sge6/bin/linux-x64/qsub -q {}'.format(node_queue),
-            batch_query_cmd='sudo -u {{username}} -E /opt/sge6/bin/linux-x64/qstat -q {} -xml'.format(node_queue),
+def get_profiles():
+    return [
+        (u'General Purpose (1 CPU)', u'cpu_lab', GridengineSpawner, dict(
+            batch_submit_cmd='sudo -u {username} -E /opt/sge6/bin/linux-x64/qsub -q cpu.q',
+            batch_query_cmd='sudo -u {username} -E /opt/sge6/bin/linux-x64/qstat -q cpu.q -xml',
             batch_cancel_cmd='sudo -u {username} -E /opt/sge6/bin/linux-x64/qdel {job_id}',
-            hub_connect_ip=hub_ip_address))
-    )
+            hub_connect_ip=hub_ip_address
+        )),
+        (u'GPU Compute (4 CPUs, 1 GPU)', u'gpu_lab', GridengineSpawner, dict(
+            batch_submit_cmd='sudo -u {username} -E /opt/sge6/bin/linux-x64/qsub -q gpu.q',
+            batch_query_cmd='sudo -u {username} -E /opt/sge6/bin/linux-x64/qstat -q gpu.q -xml',
+            batch_cancel_cmd='sudo -u {username} -E /opt/sge6/bin/linux-x64/qdel {job_id}',
+            hub_connect_ip=hub_ip_address
+        )),
+        (u'Large Memory CPU', u'mem_lab', GridengineSpawner, dict(
+            batch_submit_cmd='sudo -u {username} -E /opt/sge6/bin/linux-x64/qsub -q mem.q',
+            batch_query_cmd='sudo -u {username} -E /opt/sge6/bin/linux-x64/qstat -q mem.q -xml',
+            batch_cancel_cmd='sudo -u {username} -E /opt/sge6/bin/linux-x64/qdel {job_id}',
+            hub_connect_ip=hub_ip_address
+        ))
+    ] + get_node_profiles()
 
 
-c.ProfilesSpawner.profiles = [
-    (u'General Purpose (1 CPU)', u'cpu_lab', GridengineSpawner, dict(
-        batch_submit_cmd='sudo -u {username} -E /opt/sge6/bin/linux-x64/qsub -q cpu.q',
-        batch_query_cmd='sudo -u {username} -E /opt/sge6/bin/linux-x64/qstat -q cpu.q -xml',
-        batch_cancel_cmd='sudo -u {username} -E /opt/sge6/bin/linux-x64/qdel {job_id}',
-        hub_connect_ip=hub_ip_address
-    )),
-    (u'GPU Compute (4 CPUs, 1 GPU)', u'gpu_lab', GridengineSpawner, dict(
-        batch_submit_cmd='sudo -u {username} -E /opt/sge6/bin/linux-x64/qsub -q gpu.q',
-        batch_query_cmd='sudo -u {username} -E /opt/sge6/bin/linux-x64/qstat -q gpu.q -xml',
-        batch_cancel_cmd='sudo -u {username} -E /opt/sge6/bin/linux-x64/qdel {job_id}',
-        hub_connect_ip=hub_ip_address
-    )),
-    (u'Large Memory CPU', u'mem_lab', GridengineSpawner, dict(
-        batch_submit_cmd='sudo -u {username} -E /opt/sge6/bin/linux-x64/qsub -q mem.q',
-        batch_query_cmd='sudo -u {username} -E /opt/sge6/bin/linux-x64/qstat -q mem.q -xml',
-        batch_cancel_cmd='sudo -u {username} -E /opt/sge6/bin/linux-x64/qdel {job_id}',
-        hub_connect_ip=hub_ip_address
-    ))
-] + node_profiles
+c.ProfilesSpawner.profiles = get_profiles
 
 ## Path to SSL certificate file for the public facing interface of the proxy
 #  
