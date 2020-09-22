4167b0341911e2eca2577100004ee8a2692a9377
Daniel Treiman <dtreiman@alivecor.com>
2020-09-21 17:38:43 -0700

    Hosted domain is a list now, not a string.

--- starcluster/templates/jupyterhub.py
+++ starcluster/templates/jupyterhub.py
@@ -41,7 +41,7 @@ c.GoogleOAuthenticator.client_id = '%(oauth_client_id)s'
 c.GoogleOAuthenticator.client_secret = '%(oauth_client_secret)s'
 
 if %(hosted_domain)s is not None:
-    c.GoogleOAuthenticator.hosted_domain = %(hosted_domain)s
+    c.GoogleOAuthenticator.hosted_domain = [%(hosted_domain)s]
     c.GoogleOAuthenticator.login_service = %(login_service)s
 
 ## Whether to shutdown the proxy when the Hub shuts down.
