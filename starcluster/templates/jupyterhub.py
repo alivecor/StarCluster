464ed9632c67994750dbd56d35a0a372f85a4f2d
Daniel Treiman <dtreiman@alivecor.com>
2020-09-22 22:16:56 -0700

    Try adding --base_url to spawn command

--- starcluster/templates/jupyterhub.py
+++ starcluster/templates/jupyterhub.py
@@ -85,9 +85,9 @@ GridengineSpawner._req_homedir_default = _req_homedir_default
 
 c.JupyterHub.spawner_class = SGEProfilesSpawner
 
-#c.Spawner.cmd = ['/usr/local/bin/jupyterhub-singleuser']
+c.Spawner.cmd = ['jupyterhub-singleuser', '--base-url=/user/{username}/']
 c.Spawner.debug = True
-c.Spawner.default_url = '/lab'
+c.Spawner.default_url = '/user/{username}/lab'
 c.Spawner.notebook_dir = '%(notebook_dir)s'
 c.Spawner.environment = dict(
     SGE_ROOT='/opt/sge6',
