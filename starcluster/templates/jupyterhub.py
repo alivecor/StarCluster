1498cbed37e691d1f1d739887600962866108bb6
Daniel Treiman <dtreiman@alivecor.com>
2020-09-22 21:35:17 -0700

    Spawn jupyter-lab instead of jupyter-labhub

--- starcluster/templates/jupyterhub.py
+++ starcluster/templates/jupyterhub.py
@@ -85,9 +85,9 @@ GridengineSpawner._req_homedir_default = _req_homedir_default
 
 c.JupyterHub.spawner_class = SGEProfilesSpawner
 
-c.Spawner.cmd = ['/usr/local/bin/jupyter-labhub']
+c.Spawner.cmd = ['/usr/local/bin/jupyter-lab']
 c.Spawner.debug = True
-c.Spawner.default_url = '/tree/home/{username}'
+c.Spawner.default_url = '/lab'
 c.Spawner.notebook_dir = '%(notebook_dir)s'
 c.Spawner.environment = dict(
     SGE_ROOT='/opt/sge6',
